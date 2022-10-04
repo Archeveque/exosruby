f43785485fcd746c2d6ad06b147b33a15765ce91
archeveque <v.dasilva11d@gmail.com>
2022-10-04 13:06:39 +0200

    modifs exo_08

--- exoruby/exo_08.rb
+++ exoruby/exo_08.rb
@@ -2,7 +2,8 @@ puts"quel nombre?"
 print ">"
 wanted_number = gets.chomp.to_i
 wanted_number.times do
-      puts "===== #{wanted_number} ====="
+      puts "===== #{'%02d'%wanted_number} ====="
       wanted_number = wanted_number -1
 end
+puts "===== #{'%02d'%wanted_number} ====="
 
