5fc0eb400ab15674b78ac50d576d26ac769ce7d8
archeveque <v.dasilva11d@gmail.com>
2022-10-04 15:59:35 +0200

    modif exo.12

--- exoruby/exo_12.rb
+++ exoruby/exo_12.rb
@@ -1,13 +1,12 @@
-puts"Ton année de naissance?"
+puts"Ton age?"
 print ">"
-birthday = gets.chomp.to_i
-nb_years = 2022 - birthday
-nb_years.times do |i|
-  if nb_years - i == i then
-    puts "il y a #{nb_years - i} ans,tu avais la moitié de l'âge que tu as aujourd'hui"
+age = gets.chomp.to_i
+age.times do |i|
+  if age - i == i then
+    puts "il y a #{age - i} ans, tu avais la moitié de l'âge que tu as aujourd'hui"
   else
-  puts "Il y a #{nb_years - i} ans, tu avais #{i} ans."
+  puts "Il y a #{age - i} ans, tu avais #{i} ans."
   end
 end
 
-puts"cette année tu as eu (ou vas avoir)#{nb_years}ans"
+puts"cette année tu as eu (ou vas avoir)#{age}ans"
