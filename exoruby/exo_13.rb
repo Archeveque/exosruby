a6c86c8d67335dceacc127542e1379f3f3cff45c
archeveque <v.dasilva11d@gmail.com>
2022-10-04 23:24:51 +0200

    edits of exos 13 et 14

--- exoruby/exo_13.rb
+++ exoruby/exo_13.rb
@@ -1,6 +1,8 @@
+tab = []
 puts "nom pour l'adresse ?"
 user_mail = gets.chomp
 50.times do |nb|
   nb = nb+1
-  puts "#{user_mail}.#{'%02d'%nb}@email.com"
+  tab.append ("#{user_mail}.#{'%02d'%nb}@email.com")
 end
+puts tab
--- exoruby/exo_14.rb
+++ exoruby/exo_14.rb
@@ -1,8 +1,10 @@
+tab = []
 puts "nom pour l'adresse ?"
 user_mail = gets.chomp
 50.times do |nb|
   nb = nb+1
   if nb%2==0
-  puts "#{user_mail}.#{'%02d'%nb}@email.com"
+    tab.append ("#{user_mail}.#{'%02d'%nb}@email.com")
   end
 end
+puts tab
